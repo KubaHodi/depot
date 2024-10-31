98676acfdd417acb15c72db4638f2897007389f6
Kuba Hodi <jakhod2007@gmail.com>
2024-10-31 21:24:35 +0100

    Updated seeds.rg + index.html.erb theme renovation and functionality (tailwindcss)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 Product.delete_all
 # . . .
-Product.create!(title: 'Design and Build Great Web APIs',
-description:
+Product.create!(title: 'Design and Build Great Web APIs', description:
 %{<p>
-<em>Robust, Reliable, and Resilient</em>
+<em> Robust, Reliable, and Resilient</em>
 APIs are transforming the business world at an increasing pace. Gain
 the essential skills needed to quickly design, build, and deploy
 quality web APIs that are robust, reliable, and resilient. Go from
@@ -13,7 +12,7 @@ your API with confidence and avoid the “release into production”
 panic. Tackle just about any API challenge with more than a dozen
 open-source utilities and common programming patterns you can apply
 right away.
-</p>},
-image_url: 'maapis.jpg',
-price: 24.95)
+</p>
+}, image_url: 'maapis.jpg', price: 24.95)
 # . . .
+
