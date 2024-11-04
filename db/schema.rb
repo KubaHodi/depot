38556a4968813cf2f2ef986a1df6a193c73c44c5
Kuba Hodi <jakhod2007@gmail.com>
2024-11-04 20:47:25 +0100

    Iteration D1:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_28_204718) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_04_194324) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
