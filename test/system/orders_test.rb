673789429255cf37157fa7b6e39ae75fdf4c963d
Kuba Hodi <jakhod2007@gmail.com>
2024-11-10 21:27:56 +0100

    H2: Without playtime

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,7 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
   setup do
     @order = orders(:one)
   end
@@ -81,5 +82,40 @@ class OrdersTest < ApplicationSystemTestCase
     click_on "Destroy this order", match: :first
 
     assert_text "Order was successfully destroyed"
+  end
+
+  test "check order and delivery" do
+    LineItem.delete_all
+    Order.delete_all
+    visit store_index_url
+    click_on 'Checkout'
+
+    fill_in "Name",	with: "Dave Thomas"
+    fill_in "Address",	with: "123 Main Street"
+    fill_in "Email",	with: "dave@example.com"
+    select 'Check', from: "Pay type"
+    fill_in "Routing number",	with: "123456"
+    fill_in "Account number",	with: "987654"
+    click_button "Place Order"
+    assert_text "thank you for your order"
+    
+    perform_enqueued_jobs
+    perform_enqueued_jobs
+    assert_performedd_jobs 2
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],  mail.to
+    assert_equal "Sam Ruby <depot@example.com", mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 end
