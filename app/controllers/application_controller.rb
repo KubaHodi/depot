22a82437dbc5f48389f39eeeec5c2e05d66edc2e
Kuba Hodi <jakhod2007@gmail.com>
2024-11-14 19:39:38 +0100

    I3: repaired tests and added authorization using callback facility

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authorize
+
   allow_browser versions: :modern
+
+  protected
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
