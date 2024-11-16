22e46aa96c7de81fdac930e6fabc18dc4c575459
Kuba Hodi <jakhod2007@gmail.com>
2024-11-16 18:49:56 +0100

    J4: without playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,7 +5,11 @@ class StoreController < ApplicationController
   before_action :licznik_odwiedzin, only: [:index]
 
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end   
   private
   def licznik_odwiedzin
