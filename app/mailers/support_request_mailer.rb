620b6d8bbb98a6ba8fe860caec285e8f398911ec
KubaHodi <jakhod2007@gmail.com>
2024-12-09 13:43:18 +0100

    Fixed all tests

--- app/mailers/support_request_mailer.rb
+++ app/mailers/support_request_mailer.rb
@@ -8,6 +8,7 @@ class SupportRequestMailer < ApplicationMailer
   
   def respond(support_request)
     @support_request = support_request
+
     mail to: @support_request.email, subject: "Re: #{@support_request.subject}"
   end
   
